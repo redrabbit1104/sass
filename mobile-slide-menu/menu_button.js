e984e01a8adf3424f008265c5d5eaefd138e5aba
redrabbit1104 <redcaprabbit1104@gmail.com>
2021-05-26 19:49:39 +0900

    modify mobile-slide-menu

--- mobile-slide-menu/menu_button.js
+++ mobile-slide-menu/menu_button.js
@@ -3,7 +3,13 @@
 const btn = document.querySelector(".menu_button");
 const container = document.querySelector(".slide_container");
 const eventCondition = window.ontouchstart ? "touchstart" : "click";
-btn.addEventListener(eventCondition, () => {
-  btn.classList.toggle("open");
-  container.classList.toggle("slide");
-});
+
+btn.addEventListener(
+  eventCondition,
+  (event) => {
+    btn.classList.toggle("open");
+    container.classList.toggle("slide");
+    event.preventDefault();
+  },
+  { passive: false }
+);
